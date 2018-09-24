5ef17890194163c7b70745e57a264c85aa6ea7a0
Preston <38131912+Snypie@users.noreply.github.com>
2018-09-23 23:05:53 -0400

    Add files via upload

--- projectapi.js
+++ projectapi.js
@@ -20,16 +20,20 @@ let state = {
     ]
 }
 
-const apiUrl = 'https://na1.api.riotgames.com/lol/league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5api_key=RGAPI-936b7cb5-f720-42d8-acf0-8c7ac89a5838';
+const apiUrl = 'https://na1.api.riotgames.com/lol/league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-6537e196-1c0c-4274-a32f-9af43048e7a1';
 // const apiKey = 'RGAPI-936b7cb5-f720-42d8-acf0-8c7ac89a5838';
 // let leagueType = '/lol/league/v3/challengerleagues/by-queue/{queue}';
 
 
 let getQuoteUsingAsync = async function() {
     try {
-        const response = await fetch(apiUrl);
-        state = await response.json();
-        updateContent();
+        fetch(apiUrl, {
+
+            mode: 'no-cors'
+        
+        });
+        
+        // state = await response.json();
     } catch (err) {
         console.log('Something went wrong. :(');
         console.log(err);
